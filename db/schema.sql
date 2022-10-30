691f77da38b1146340d4c21c0b75b13e5e1d6df8
Mark Gardner <themarkgardner@gmail.com>
2022-10-30 16:53:46 -0500

    refactored sql queries to use prepared statements

--- db/schema.sql
+++ db/schema.sql
@@ -7,12 +7,12 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     deptName VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE empRole (
-    id INT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
@@ -22,7 +22,7 @@ CREATE TABLE empRole (
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
