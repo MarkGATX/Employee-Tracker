8b2a90e98ece2c1c79c164bfd0420f8e10f4db8e
Mark Gardner <themarkgardner@gmail.com>
2022-10-30 19:19:49 -0500

    Added and tested function to return list of employees by Manager

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ function selectAction() {
             type: 'list',
             message: 'What would you like to do?',
             name: 'action',
-            choices: ['View all departments', 'View all employee roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an Employee Role','Update Employee Manager']
+            choices: ['View all departments', 'View all employee roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an Employee Role','Update Employee Manager', 'View Employees by Manager']
         },
     ])
         .then((response) => {
@@ -53,6 +53,9 @@ function selectAction() {
                     break;
                 case 'Update Employee Manager':
                     employees.updateEmpMgr();
+                    break;
+                case 'View Employees by Manager':
+                    employees.viewEmpByMgr();
                     break;
                 default:
                     console.log('Please make a valid selection');
